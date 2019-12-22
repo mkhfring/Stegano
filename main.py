9ec8bac1fb90f36e7b53e200ef65f98c1407bb9b
mkhfring <m.khajezade@carrene.com>
2019-12-22 22:33:35 +0330

    fixing the CLI

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from stegano import lsb
 
 if __name__ == '__main__':
-    image_location = './data/stegosaurus.jpg'
+    image_location = './data/1.jpg'
     secret = lsb.hide(image_location, "سلام")
-    secret.save("./data/Lenna-secret.png")
-    clear_message = lsb.reveal("./data/Lenna-secret.png")
+    secret.save("test.png")
+    clear_message = lsb.reveal("./test.png")
+    print(clear_message)
+
     assert 1 == 1
